9f5336d3f9aea54a6dc149fd8c4007b21740fff4
minatofujimoto <minatofujimototrc@gmail.com>
2025-03-12 10:36:17 +0900

    feat: add unit conversion analytics

--- components/DecisionsPanel.tsx
+++ components/DecisionsPanel.tsx
@@ -112,3 +112,4 @@ export default function DecisionsPanel({ decisions }: DecisionsPanelProps) {
   );
 }
 // Unit feature implementation - 20250310_0001
+// Unit feature implementation - 20250312_0006
