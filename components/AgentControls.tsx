ae8c6262482dce399772e2299bca46fece9a8501
minatofujimoto <minatofujimototrc@gmail.com>
2025-04-07 20:49:26 +0900

    feat: implement unit conversion caching

--- components/AgentControls.tsx
+++ components/AgentControls.tsx
@@ -56,3 +56,4 @@ export default function AgentControls({ isRunning, onToggle, onRefresh }: AgentC
   );
 }
 // Unit feature implementation - 20250325_0026
+// Unit feature implementation - 20250407_0044
