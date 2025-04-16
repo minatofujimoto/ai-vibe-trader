c9fae22694059bf8fdeeae6c7bb1fa0d4fe01c18
minatofujimoto <minatofujimototrc@gmail.com>
2025-04-16 14:48:28 +0900

    feat: implement unit conversion caching

--- components/DecisionsPanel.tsx
+++ components/DecisionsPanel.tsx
@@ -114,3 +114,4 @@ export default function DecisionsPanel({ decisions }: DecisionsPanelProps) {
 // Unit feature implementation - 20250310_0001
 // Unit feature implementation - 20250312_0006
 // Unit feature implementation - 20250414_0059
+// Unit feature implementation - 20250416_0066
