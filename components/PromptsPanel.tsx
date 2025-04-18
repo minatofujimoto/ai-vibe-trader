c288355e15dba561e82a30b923d63bc5e8e5689d
minatofujimoto <minatofujimototrc@gmail.com>
2025-04-18 11:45:05 +0900

    refactor: improve unit conversion maintainability

--- components/PromptsPanel.tsx
+++ components/PromptsPanel.tsx
@@ -83,3 +83,4 @@ export default function PromptsPanel({ prompts }: PromptsPanelProps) {
 // Unit feature implementation - 20250408_0047
 // Unit feature implementation - 20250416_0064
 // Unit feature implementation - 20250417_0068
+// Unit feature implementation - 20250418_0071
