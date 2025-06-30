25ba6f7a1677e678fd8a4715b540f8d96b6191bb
minatofujimoto <minatofujimototrc@gmail.com>
2025-06-30 23:32:48 +0900

    test: add unit tests for conversion functions

--- components/AgentControls.tsx
+++ components/AgentControls.tsx
@@ -65,3 +65,4 @@ export default function AgentControls({ isRunning, onToggle, onRefresh }: AgentC
 // Unit feature implementation - 20250516_0126
 // Unit feature implementation - 20250529_0147
 // Unit feature implementation - 20250613_0173
+// Unit feature implementation - 20250630_0194
