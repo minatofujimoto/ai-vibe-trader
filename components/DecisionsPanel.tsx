a9aec0c553151561583982a427b5c490aebf809d
minatofujimoto <minatofujimototrc@gmail.com>
2025-07-01 09:06:21 +0900

    refactor: optimize unit conversion performance

--- components/DecisionsPanel.tsx
+++ components/DecisionsPanel.tsx
@@ -119,3 +119,4 @@ export default function DecisionsPanel({ decisions }: DecisionsPanelProps) {
 // Unit feature implementation - 20250602_0154
 // Unit feature implementation - 20250611_0169
 // Unit feature implementation - 20250616_0174
+// Unit feature implementation - 20250701_0195
